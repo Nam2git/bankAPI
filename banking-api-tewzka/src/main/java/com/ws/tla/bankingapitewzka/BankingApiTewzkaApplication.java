1bd9240a3b0f968e00f115015f9369d98b999877
namytech <namrata2683@yahoo.com>
2023-08-10 22:28:00 -0500

    Spring project setup for Banking api

---
+++ banking-api-tewzka/src/main/java/com/ws/tla/bankingapitewzka/BankingApiTewzkaApplication.java
@@ -0,0 +1,13 @@
+package com.ws.tla.bankingapitewzka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingApiTewzkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingApiTewzkaApplication.class, args);
+	}
+
+}
